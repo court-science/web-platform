69e5b6a2b8b4d766519c10ae491d46cf037f155f
Aditya-Ohri <ohri.aditya@yahoo.ca>
2019-07-20 01:01:51 -0400

    Front-end config

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello Court Science Fans!!!!'
+def output():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
