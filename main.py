6ee5c3363bec336a3caef68d9a192e860724e483
Aditya-Ohri <ohri.aditya@yahoo.ca>
2019-07-23 22:59:14 -0400

    HTTP

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def output():
     return render_template('index.html')
+
+
+@app.route('/main', methods = ['POST'])
+def main():
+    user_csv = request.form['']
 
 
 if __name__ == '__main__':
