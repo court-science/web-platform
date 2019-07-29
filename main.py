174cbf4c0bee23af4eb9087e6a3e07771d2d133f
robuntu <robuntu@stro.ycdsb.ca>
2019-07-29 12:13:37 -0400

    backend request handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+from spider import court_science_magic
 
 app = Flask(__name__)
 
@@ -7,9 +8,17 @@ def output():
     return render_template('index.html')
 
 
-@app.route('/main', methods = ['POST'])
+@app.route('/main', methods = ['POST', 'GET'])
 def main():
-    user_csv = request.form['']
+    if request.method == 'POST':
+        user_csv = request.form['myCSV']
+        print(user_csv)
+        user_stats = []
+        for i in range(11):
+            if request.form['stat' + str(i + 1)] != 'false':
+                user_stats.append(request.form['stat' + str(i + 1)])
+        print(user_stats)
+    return "Hang in there cs fans!"
 
 
 if __name__ == '__main__':
