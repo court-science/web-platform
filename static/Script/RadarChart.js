a942580143b1de62ed0827498d236248f9d5f5c0
dbriskin <dan.briskin@mlse.com>
2020-05-17 18:44:32 -0400

    adding table functions and radar chart configurations

--- static/Script/RadarChart.js
+++ static/Script/RadarChart.js
@@ -7,30 +7,20 @@ var RadarChart = {
           cfg[i] = options[i];
         }
       }
-    }
+    }  
 
-    // d3.select(id).select("svg").remove();
-    
-    // var g = d3.select(id)
-    //         .append("svg")
-    //         .attr("width", cfg.w+cfg.ExtraWidthX)
-    //         .attr("height", cfg.h+cfg.ExtraWidthY)
-    //         .append("g")
-    //         .attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
-    //         ;
-    console.log("THE DATA TO SHOW IN RADAR:",d);
-    var g = d3.select("svg");
-    console.log("THE SELECTED SVG's HEIGHTIS:",g.attr("height"));
-    console.log("THE SELECTED SVG's WIDTH IS:",g.attr("width"));
-
-    var cfg = {
+    var g = d3.select("#chart-div").select("svg");//.attr("width",400).attr("height",350);
+    // console.log("NEW SVG HEIGHT IS:",g.attr("height"))
+    // console.log("NEW SVG WIDTH IS:",g.attr("width"))
+    var margin = {top: 30, right: 20, bottom: 30, left: 50};
+    cfg = {
       radius: 5,
-      h: +g.attr("height"),
-      w: +g.attr("height"),
+      h: 450,//+g.attr("height"),
+      w: 500,//+g.attr("width"),
       factor: 1,
       factorLegend: .85,
       levels: 3,
-      maxValue: 0,
+      maxValue: 1,
       radians: 2 * Math.PI,
       opacityArea: 0.5,
       ToRight: 5,
@@ -46,7 +36,7 @@ var RadarChart = {
      var allAxis = (d[0].map(function(i, j){return i.axis}));
      var total = allAxis.length;
      var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
-     var Format = d3.format('%');
+     var Format = d3.format('.1%');
      
      g
       .attr("width", cfg.w+cfg.ExtraWidthX)
@@ -72,25 +62,25 @@ var RadarChart = {
        .style("stroke", "grey")
        .style("stroke-opacity", "0.75")
        .style("stroke-width", "0.3px")
-       .attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");
+       .attr("transform", "translate(" + (cfg.w/2-levelFactor + margin.left) + ", " + (cfg.h/2-levelFactor + margin.top) + ")");
     }
 
     //Text indicating at what % each level is
-    for(var j=0; j<cfg.levels; j++){
-      var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
-      g.selectAll(".levels")
-       .data([1]) //dummy data
-       .enter()
-       .append("svg:text")
-       .attr("x", function(d){return levelFactor*(1-cfg.factor*Math.sin(0));})
-       .attr("y", function(d){return levelFactor*(1-cfg.factor*Math.cos(0));})
-       .attr("class", "legend")
-       .style("font-family", "sans-serif")
-       .style("font-size", "10px")
-       .attr("transform", "translate(" + (cfg.w/2-levelFactor + cfg.ToRight) + ", " + (cfg.h/2-levelFactor) + ")")
-       .attr("fill", "#737373")
-       .text(Format((j+1)*cfg.maxValue/cfg.levels));
-    }
+    // for(var j=0; j<cfg.levels; j++){
+    //   var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+    //   g.selectAll(".levels")
+    //    .data([1]) //dummy data
+    //    .enter()
+    //    .append("svg:text")
+    //    .attr("x", function(d){return levelFactor*(1-cfg.factor*Math.sin(0));})
+    //    .attr("y", function(d){return levelFactor*(1-cfg.factor*Math.cos(0));})
+    //    .attr("class", "legend")
+    //    .style("font-family", "sans-serif")
+    //    .style("font-size", "10px")
+    //    .attr("transform", "translate(" + (cfg.w/2-levelFactor + cfg.ToRight) + ", " + (cfg.h/2-levelFactor) + ")")
+    //    .attr("fill", "#737373")
+    //    .text(Format((j+1)*cfg.maxValue/cfg.levels));
+    // }
     
     series = 0;
 
@@ -105,6 +95,7 @@ var RadarChart = {
         .attr("y1", cfg.h/2)
         .attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
         .attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .attr("class", "line")
         .style("stroke", "grey")
         .style("stroke-width", "1px");
@@ -116,7 +107,7 @@ var RadarChart = {
         .style("font-size", "11px")
         .attr("text-anchor", "middle")
         .attr("dy", "1.5em")
-        .attr("transform", function(d, i){return "translate(0, -10)"})
+        .attr("transform", "translate(" + margin.left + "," + (margin.top + -10) + ")")
         .attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
         .attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
 
@@ -132,35 +123,39 @@ var RadarChart = {
         });
       dataValues.push(dataValues[0]);
       g.selectAll(".area")
-                     .data([dataValues])
-                     .enter()
-                     .append("polygon")
-                     .attr("class", "radar-chart-serie"+series)
-                     .style("stroke-width", "2px")
-                     .style("stroke", cfg.color(series))
-                     .attr("points",function(d) {
-                         var str="";
-                         for(var pti=0;pti<d.length;pti++){
-                             str=str+d[pti][0]+","+d[pti][1]+" ";
-                         }
-                         return str;
-                      })
-                     .style("fill", function(j, i){return cfg.color(series)})
-                     .style("fill-opacity", cfg.opacityArea)
-                     .on('mouseover', function (d){
-                                        z = "polygon."+d3.select(this).attr("class");
-                                        g.selectAll("polygon")
-                                         .transition(200)
-                                         .style("fill-opacity", 0.1); 
-                                        g.selectAll(z)
-                                         .transition(200)
-                                         .style("fill-opacity", .7);
-                                      })
-                     .on('mouseout', function(){
-                                        g.selectAll("polygon")
-                                         .transition(200)
-                                         .style("fill-opacity", cfg.opacityArea);
-                     });
+        .data([dataValues])
+        .enter()
+        .append("polygon")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "radar-chart-serie"+series)
+        .style("stroke-width", "2px")
+        .style("stroke", cfg.color(series))
+        .attr("points",function(d) {
+            var str="";
+            for(var pti=0;pti<d.length;pti++){
+                str=str+d[pti][0]+","+d[pti][1]+" ";
+            }
+            return str;
+        })
+        .style("fill", function(j, i){return cfg.color(series)})
+        .style("fill-opacity", cfg.opacityArea);
+
+      g.selectAll(".area")
+        .on('mouseover', function (d){
+                          z = "polygon."+d3.select(this).attr("class");
+                          g.selectAll("polygon")
+                            .transition(200)
+                            .style("fill-opacity", 0.1); 
+                          g.selectAll(z)
+                            .transition(200)
+                            .style("fill-opacity", .7);
+                          console.log(d)
+                        })
+        .on('mouseout', function(){
+                          g.selectAll("polygon")
+                            .transition(200)
+                            .style("fill-opacity", cfg.opacityArea);
+        });
       series++;
     });
     series=0;
@@ -171,6 +166,7 @@ var RadarChart = {
         .data(y).enter()
         .append("svg:circle")
         .attr("class", "radar-chart-serie"+series)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .attr('r', cfg.radius)
         .attr("alt", function(j){return Math.max(j.value, 0)})
         .attr("cx", function(j, i){
@@ -189,13 +185,6 @@ var RadarChart = {
                     newX =  parseFloat(d3.select(this).attr('cx')) - 10;
                     newY =  parseFloat(d3.select(this).attr('cy')) - 5;
                     
-                    tooltip
-                        .attr('x', newX)
-                        .attr('y', newY)
-                        .text(Format(d.value))
-                        .transition(200)
-                        .style('opacity', 1);
-                        
                     z = "polygon."+d3.select(this).attr("class");
                     g.selectAll("polygon")
                         .transition(200)
@@ -203,6 +192,13 @@ var RadarChart = {
                     g.selectAll(z)
                         .transition(200)
                         .style("fill-opacity", .7);
+                    tooltip
+                        .attr('x', newX)
+                        .attr('y', newY)
+                        .text(Format(d.value))
+                        .transition(200)
+                        .style('opacity', 1);
+                    console.log(d)
                   })
         .on('mouseout', function(){
                     tooltip
@@ -222,7 +218,56 @@ var RadarChart = {
                .style('opacity', 0)
                .style('font-family', 'sans-serif')
                .style('font-size', '13px');
+
+    ////////////////////////////////////////////
+    /////////// Initiate legend ////////////////
+    ////////////////////////////////////////////
+    var LegendOptions = playerNames;
+    
+   g
+    .select('svg')
+    .attr("width", cfg.w+300)
+    .attr("height", cfg.h)
+
+    //Create the title for the legend
+    var text = g.append("text")
+    .attr("class", "legend-title")
+    .attr('transform', 'translate(70,0)') 
+    .attr("x", cfg.w - 110)
+    .attr("y", 10)
+    .text("Players:");
+      
+    //Initiate Legend	
+    var legend = g.append("g")
+    .attr("class", "legend")
+    .attr("height", 100)
+    .attr("width", 200)
+    .attr('transform', 'translate(70,20)') 
+    ;
+    //Create colour squares
+    legend.selectAll('rect')
+      .data(LegendOptions)
+      .enter()
+      .append("rect")
+      .attr("x", cfg.w - 105)
+      .attr("y", function(d, i){ return i * 20;})
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", function(d, i){ return cfg.color(i);})
+      ;
+    //Create text next to squares
+    legend.selectAll('text')
+      .data(LegendOptions)
+      .enter()
+      .append("text")
+      .attr("x", cfg.w - 92)
+      .attr("y", function(d, i){ return i * 20 + 9;})
+      .attr("font-size", "11px")
+      .attr("fill", "#737373")
+      .text(function(d) { return d; })
+      ;	
   }
+  
 };
 
 function normalizeObjects(data) {
---
+++ static/Script/tableFunctions.js
@@ -0,0 +1,22 @@
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+function drawSampleTable() {
+    var table_data = new google.visualization.DataTable();
+
+        table_data.addColumn('string', 'Name');
+        table_data.addColumn('number', 'PTS');
+        table_data.addColumn('number', 'REB');
+        table_data.addColumn('number', 'AST');
+        table_data.addRows([
+            ['Lebron James',  26.4, 8.1, 10.7],
+            ['Kawhi Leonard',   28.2,  9.3, 4.2],
+            ['Kevin Durant', 30.1, 8.5, 5.6],
+            ['Stephen Curry',   29.5,  5.5, 8.3]
+        ]);
+
+    
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+    
+    table.draw(table_data, {showRowNumber: true, width: '100%', height: '100%'});
+    }
