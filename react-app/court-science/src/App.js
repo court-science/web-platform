4ef41412702e49e22560de144d99f14eebc17618
Dan Briskin <Dan.Briskin@MLSE.com>
2020-05-26 00:00:45 -0400

    d3 package and radar chart rendered in App.js

--- react-app/court-science/src/App.js
+++ react-app/court-science/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RadarChart from './RadarChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WELCOME TO COURT SCIENCE IN REACT.JS!!!
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width="550" height="550"> 
+        <RadarChart width={450} height={450}/>
+      </svg>
     </div>
   );
 }
