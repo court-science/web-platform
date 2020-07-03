70ed1763fe08ad437db380440cd7fc56392df5d8
Andrew Tanner <atanner2727@gmail.com>
2020-07-03 16:50:37 -0400

    changeS

--- django/bootcamp/urls.py
+++ django/bootcamp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
-from .views import HomePageView, CampPageView, LoginPageView, ZonePageView
+from .views import HomePageView, CampPageView, LoginPageView, ZonePageView, SignupPageView
 
 urlpatterns = [
     #path('', homePageView, name='home')
     path('', HomePageView.as_view(), name='index'),
     path('home', HomePageView.as_view(), name='home'),
     path('camp', CampPageView.as_view(), name='camp'),
+    path('signup', SignupPageView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('zone', ZonePageView.as_view(), name='zone'),
     # path('login', auth_views.login,  {'template_name': 'templates/login.html'}, name='login')
--- django/bootcamp/views.py
+++ django/bootcamp/views.py
@@ -6,11 +6,16 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
-class CampPageView(TemplateView):
-    template_name = 'camp.html'
+class SignupPageView(TemplateView):
+    template_name = 'signup.html'
 
 class LoginPageView(TemplateView):
     template_name = 'login.html'
 
 class ZonePageView(TemplateView):
     template_name = 'zone.html'
+
+class CampPageView(TemplateView):
+    template_name = 'camp.html'
+
+    
