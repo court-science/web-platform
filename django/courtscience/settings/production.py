6a9738d87c0b94379f28785a132ff84cb49c0ad2
Andrew Tanner <atanner2727@gmail.com>
2020-07-20 01:05:04 -0400

    add propogation for errors

--- django/courtscience/settings/production.py
+++ django/courtscience/settings/production.py
@@ -35,6 +35,7 @@ DATABASES = {
 INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
 ALLOWED_HOSTS = ["*"]
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
