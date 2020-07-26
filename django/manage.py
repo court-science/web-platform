09ca2bebf60307c68d8c1d7ccc588fdc569b61bc
Aditya-Ohri <ohri.aditya@yahoo.ca>
2020-07-25 23:41:24 -0400

    camp

--- django/manage.py
+++ django/manage.py
@@ -6,7 +6,7 @@ import dotenv
 
 def main():
     dotenv.read_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courtscience.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courtscience.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
